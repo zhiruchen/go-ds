8029b870ca884a1061a604b3040ee9520f76599a
zhiruchen <zhiruchen1992@foxmail.com>
2018-03-17 09:42:06 +0800

    add algs

---
+++ algs/merge_sort.go
@@ -0,0 +1,46 @@
+package algs
+
+
+// MergeSort stable sort algorithm
+// https://zh.wikipedia.org/wiki/%E5%BD%92%E5%B9%B6%E6%8E%92%E5%BA%8F
+func MergeSort(outOrderSeq []int32) []int32 {
+	if len(outOrderSeq) < 2 {
+		return outOrderSeq
+	}
+
+	mid := len(outOrderSeq) / 2
+	leftSeq := MergeSort(outOrderSeq[0:mid])
+	rightSeq := MergeSort(outOrderSeq[mid:])
+
+	return merge(leftSeq, rightSeq)
+}
+
+// merge two sorted array
+func merge(seq1, seq2 []int32) []int32 {
+	i,j := 0, 0
+	len1, len2 := len(seq1), len(seq2)
+
+	var seq []int32
+
+	for i < len1 && j < len2 {
+		if seq1[i] <= seq2[j] {
+			seq = append(seq, seq1[i])
+			i++
+		} else {
+			seq = append(seq, seq2[j])
+			j++
+		}
+	}
+
+	for i < len1 {
+		seq = append(seq, seq1[i])
+		i++
+	}
+
+	for j < len2 {
+		seq = append(seq, seq2[j])
+		j++
+	}
+
+	return seq
+}
---
+++ algs/merge_sort_test.go
@@ -0,0 +1,37 @@
+package algs
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestMergeSort(t *testing.T) {
+	testCases := []struct{
+		input []int32
+		output []int32
+	}{
+		{
+			input: []int32{1,2,3,4,5},
+			output: []int32{1,2,3,4,5},
+		},
+		{
+			input: []int32{7,3,2,5,1,6},
+			output: []int32{1,2,3,5,6,7},
+		},
+		{
+			input: []int32{7,3,2,5,1,6,0},
+			output: []int32{0,1,2,3,5,6,7},
+		},
+		{
+			input: []int32{100},
+			output: []int32{100},
+		},
+	}
+
+	for _, cc := range testCases {
+		result := MergeSort(cc.input)
+		if !reflect.DeepEqual(result, cc.output) {
+			t.Errorf("expected: %v, get: %v\n", cc.output, result)
+		}
+	}
+}
